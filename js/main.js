17dbd9e83f90ec045688bb6caf8ea7a385354787
khaker-uzz <toshtemirovulugbek008@gmail.com>
2023-11-11 15:17:04 +0500

    java

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var hello = String(prompt("Assalomu alekum"))
+console.log(hello);
+
+var howareyou = String(prompt("yaxshimsz"))
+console.log(howareyou);
+
+var namee = String(prompt("ismingiz nma"))
+console.log(namee);
+
+var username = String(prompt("familiyangiz nima"))
+console.log(username);
+
+var maqsad = String(prompt("hayotdagi maqsadingiz"))
+console.log(maqsad);
+
+var Doc = String(prompt("yusufga kvarterani kalitini berasizmi"))
+console.log(Doc);
+
+var Data = Number(prompt("aldamang bugun boramiz"))
+console.log(Data);
+
+var yoshingiz = Number(prompt("aldamang bugun boramiz"))
+console.log(yoshingiz);
+
+var korinmaysiz = Number(prompt("ko'rinmaysiz"))
+console.log(korinmaysiz);
+
+
+
+var Javob = [
+    "salom: " + " " + hello + "\n" +
+    "yaxshimisz: " + " " + howareyou + "\n" +
+    "ismingiz nma: " + " " + namee + "\n" +
+    "familiyangiz nma: " + " " + username + "\n" +
+    "maqsadingiz: " + " " + maqsad + "\n" +
+    'kalitni berasizmi: ' + " " + Doc + "\n" +
+    'aldamang: ' + " " + Doc + "\n" +
+    'boraylikmi: ' + " " + Data + "\n" +
+    "yoshingiz: " + " " + yoshingiz + "\n" +
+    "ko'rinmaysiz : " + " " + korinmaysiz + "\n" 
+]
+
+
+alert(Javob)
